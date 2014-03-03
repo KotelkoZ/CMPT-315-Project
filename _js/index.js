79f7dc8b561bc9d85ffbabf22bda4dfd92754ddd
howorkol <lhoworko@gmail.com>
2014-03-03 12:31:43 -0700

    Stock.html page Graph
    The graph on the second page works, can add more companies to the graph
    for comparison. Pull data from local files to stop querying quandl so
    often.

--- _js/index.js
+++ _js/index.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     .css('cursor', 'pointer');
                 
     // Set the curser to a pointer when hovering h3#filter_btn
-    $('h3#filter_btn').click(makeChart)
+    $('h3#filter_btn').click(function() { makeChart(companyObject); })
     .css('cursor', 'pointer');
                 
     // Values for the range sliders.
@@ -41,15 +41,13 @@ $(document).ready(function() {
         var s = (max - min) / 3;
         var p1 = removeZero(parseFloat(min + s).toFixed(1));
         var p2 = removeZero(parseFloat(max - s).toFixed(1));
-                    
         
-    
         $("#" + slider_id).slider({
             from: min,  to: max,
             scale: [min, '|', p1, '|', p2, '|', max],
             limits: false,
             step: (max - min) / 20,
-            onstatechange: makeChart
+            onstatechange: function() { makeChart(companyObject); }
         });
     });
 }); // End JQuery
@@ -60,15 +58,13 @@ google.load("visualization", "1", {packages:["corechart"]});
             
 var companyObject;
 var dataTable;
-var isDone = false;
-            
+
 function done() {
     // Get the data from yahoo
-    isDone = true;
     $.getJSON('http://query.yahooapis.com/v1/public/yql?q=select%20Symbol%2CBookValue%2CYearLow%2CYearHigh%2CChangeFromYearLow%2CChangeFromYearHigh%2CName%2COpen%2CPreviousClose%2CChangeinPercent%2CPERatio%2CVolume%2CDividendYield%2CStockExchange%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22SRCE%22%2C%22MMM%22%2C%22ABM%22%2C%22AFL%22%2C%22APD%22%2C%22MO%22%2C%22AWR%22%2C%22ADM%22%2C%22T%22%2C%22ATO%22%2C%22ADP%22%2C%22BDX%22%2C%22BMS%22%2C%22BKH%22%2C%22BRC%22%2C%22BF-B%22%2C%22BCR%22%2C%22CWT%22%2C%22CSL%22%2C%22CVX%22%2C%22CB%22%2C%22CINF%22%2C%22CTAS%22%2C%22CLC%22%2C%22CLX%22%2C%22KO%22%2C%22CL%22%2C%22CBSH%22%2C%22CTBI%22%2C%22CSVI%22%2C%22CTWS%22%2C%22ED%22%2C%22DBD%22%2C%22DCI%22%2C%22DOV%22%2C%22EFSI%22%2C%22EV%22%2C%22EMR%22%2C%22EGN%22%2C%22XOM%22%2C%22FDO%22%2C%22FRT%22%2C%22BEN%22%2C%22GPC%22%2C%22GRC%22%2C%22FUL%22%2C%22HCP%22%2C%22HP%22%2C%22HRL%22%2C%22ITW%22%2C%22JNJ%22%2C%22KMB%22%2C%22LANC%22%2C%22LEG%22%2C%22LOW%22%2C%22MKC%22%2C%22MCD%22%2C%22MHFI%22%2C%22MDT%22%2C%22MCY%22%2C%22MGEE%22%2C%22MSEX%22%2C%22MSA%22%2C%22NC%22%2C%22NFG%22%2C%22NDSN%22%2C%22NWN%22%2C%22NUE%22%2C%22ORI%22%2C%22PH%22%2C%22PNR%22%2C%22PEP%22%2C%22PNY%22%2C%22PPG%22%2C%22PG%22%2C%22STR%22%2C%22RAVN%22%2C%22RLI%22%2C%22RPM%22%2C%22SHW%22%2C%22SIAL%22%2C%22SJW%22%2C%22SON%22%2C%22SWK%22%2C%22SCL%22%2C%22SYY%22%2C%22TROW%22%2C%22TGT%22%2C%22TDS%22%2C%22TNC%22%2C%22TMP%22%2C%22TR%22%2C%22UGI%22%2C%22UBSI%22%2C%22UVV%22%2C%22UHT%22%2C%22VAL%22%2C%22VVC%22%2C%22VFC%22%2C%22GWW%22%2C%22WAG%22%2C%22WMT%22%2C%22WEYS%22%2C%22WGL%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', 
             function(file) {
         companyObject = file.query.results.quote;
-        makeChart();
+        makeChart(companyObject);
     });
 }
             
@@ -104,7 +100,7 @@ function satisfyFilters(c) {
 }
             
 function makeChart() {
-    if (!isDone) return;
+    if (companyObject === undefined) return;
                 
     var e = document.getElementById('XSelect');
     var xValue = e.options[e.selectedIndex];
@@ -142,13 +138,12 @@ function makeChart() {
     };
 
     // Draw the chart.
-    chart = new google.visualization.BubbleChart(document.getElementById('chart_div'));
+    chart = new google.visualization.BubbleChart(document.getElementById('main_div'));
     chart.draw(dataTable, options);
-                google.visualization.events.addListener(chart, 'select', selectHandler);
+    google.visualization.events.addListener(chart, 'select', selectHandler);
                 
     // Bubble selection handler. Calls search on the ticker symbol of the bubble that was chosen.
     function selectHandler() {
-        console.log('click');
         var row = chart.getSelection()["0"].row;
         var symbol = dataTable.getValue(row, 0);
         search(symbol);
