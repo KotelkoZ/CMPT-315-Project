cbdc616c3b9d0d26e4d53da2bf5dc3738931e16a
BodkerJ <j.bodker@hotmail.com>
2014-03-05 20:25:28 -0700

    completed java to make master .json
    .json takes the lastest data from each company.

--- QuandlQuery/src/quandlquery/QuandlQuery.java
+++ QuandlQuery/src/quandlquery/QuandlQuery.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
@@ -24,16 +25,12 @@ public class QuandlQuery {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
         QuandlQuery obj = new QuandlQuery();
-        //obj.run();
-        obj.createJSON();
-        obj.readJSON();
-        obj.addToJSON();
-        
+        obj.run();        
     }
     
-    public void run() throws FileNotFoundException, IOException {
+    public void run() throws FileNotFoundException, IOException, ParseException {
         
         
         String csvFile = "stockinfo.csv";
@@ -53,6 +50,7 @@ public class QuandlQuery {
             System.out.println(ticker);
             
             if(i < 10) {
+                
                 try {
                     String url = "http://www.quandl.com/api/v1/datasets/OFDP/DMDRN_" + ticker + "_ALLFINANCIALRATIOS.json?auth_token=iT1LrBo1Uw79uqJfrKyb";
                     URL website = new URL(url);
@@ -62,6 +60,13 @@ public class QuandlQuery {
                 } catch(FileNotFoundException e) {
                     System.out.println("^There was an exception! The url doesn't exist");
                 }
+                
+                if(i == 0) {
+                    createJSON(ticker);
+                }
+                
+                readJSON(ticker);
+                
             }
             i++;
         }
@@ -70,46 +75,90 @@ public class QuandlQuery {
         
     }
     
-    public void readJSON() {
+    public void readJSON(String ticker) throws IOException, ParseException {
         
         JSONParser parser = new JSONParser();
         
         try {
             
-            Object obj = parser.parse(new FileReader("tickers/AAPL.json"));
+            Object obj = parser.parse(new FileReader("tickers/" + ticker + ".json"));
             
             JSONObject jsonObject = (JSONObject) obj;
- 
-            String name = (String) jsonObject.get("name");
-            System.out.println(name);
-
-
-            // loop array
+            
             JSONArray data = (JSONArray) jsonObject.get("data");
             Iterator<JSONArray> iterator = data.iterator();
             data = iterator.next();
-            System.out.println(data);
             
-            JSONArray ticker = new JSONArray();
-            ticker.add(name);
-            ticker.add(data);
+            JSONArray company = new JSONArray();
+            company.add(ticker);
+            company.add(data);
             
-            System.out.println(ticker);
+            addToJSON(company, ticker);
             
-        } catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	} catch (ParseException e) {
-		e.printStackTrace();
-	}
+        } catch(FileNotFoundException e) {
+                    System.out.println("^There was an exception! The url doesn't exist");
+                }
+        
     }
     
-    public void createJSON() throws FileNotFoundException {
-        FileOutputStream fos = new FileOutputStream("alltickers.json");
+    public void createJSON(String ticker) throws FileNotFoundException, IOException, ParseException {
+        
+        JSONObject json = new JSONObject();
+        JSONParser parser = new JSONParser();
+        
+        try {
+            
+            Object obj = parser.parse(new FileReader("tickers/" + ticker + ".json"));
+            
+            JSONObject jsonObject = (JSONObject) obj;
+
+            JSONArray columns = (JSONArray) jsonObject.get("column_names");
+            
+            json.put("column_names", columns);
+            
+        } catch(FileNotFoundException e) {
+                    System.out.println("^There was an exception! The url doesn't exist");
+                }
+        
+        try {
+            
+            FileWriter file = new FileWriter("alltickers.json");
+            file.write(json.toJSONString());
+            file.flush();
+            file.close();
+            
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        
     }
     
-    public void addToJSON() {
+    public void addToJSON(JSONArray company, String ticker) throws IOException, ParseException {
+        
+        JSONObject json = new JSONObject();
+        JSONParser parser = new JSONParser();
+        
+        try {
+            
+            Object obj = parser.parse(new FileReader("alltickers.json"));
+            
+            json = (JSONObject) obj;
+            
+            json.put(ticker, company);
+            
+        } catch(FileNotFoundException e) {
+                    System.out.println("^There was an exception! The url doesn't exist");
+                }
+        
+        try {
+            
+            FileWriter file = new FileWriter("alltickers.json");
+            file.write(json.toJSONString());
+            file.flush();
+            file.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
         
     }
     
