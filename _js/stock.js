a7874883e84e82c51ca3a35c3797f27b47521bf3
KotelkoZ <kotelkoz@gmail.com>
2014-03-11 15:42:01 -0600

    somethings

--- _js/stock.js
+++ _js/stock.js
@@ -13,9 +13,33 @@ $(document).ready(function() {
             combo.innerHTML += '<option>' + column_names[i] + '</option>';
         }
         makeChart();
+        makeTable();
     });
 });  
             
+function makeTable(){
+    google.load('visualization', '1', {packages:['table']});
+    google.setOnLoadCallback(drawTable);
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Attribute');
+          /*for(var i = 0;i < document.getElementById('combo').length;i++){
+              data.addRow('string', document.getElementById('combo').value);
+          }*/
+        data.addColumn('number', 'stuff';
+        data.addColumn('boolean', 'Full Time Employee');
+        data.addRows([
+          ['Mike',  {v: 10000, f: '$10,000'}, true],
+          ['Jim',   {v:8000,   f: '$8,000'},  false],
+          ['Alice', {v: 12500, f: '$12,500'}, true],
+          ['Bob',   {v: 7000,  f: '$7,000'},  true]
+        ]);
+
+        var table = new google.visualization.Table(document.getElementById('stock_compare_table'));
+        table.draw(data, {});
+
+}
+
 function makeChart() {
     var data = new google.visualization.DataTable();
                     
