f5c0a3c5d09771178144a523903c353f6876077b
howorkol <lhoworko@gmail.com>
2014-03-11 21:34:23 -0600

    Updated Directory Checks
    Now check if specific stocks exist before serving them up to avoid a
    broken page. Created a function that creates error functions. Used it to
    make send404 and send500 methods.

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var server = http.Server(function(req, res) {
     var url = parse(req.url);
     var filepath = path.join(root, url.pathname);
     
+    var send404 = createError(404, 'File Not Found.');
+    var send500 = createError(500, 'Internal Server Error.');
+    
     if (url.pathname == '/') {
         res.writeHead(200, {"Content-Type": "text/html"});
         var stream = fs.createReadStream(root + '/_html/index.html');
@@ -23,36 +26,42 @@ var server = http.Server(function(req, res) {
     }
             
     else if (url.pathname.search('/stock/') == 0) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        var stream = fs.createReadStream(root + '/_html/stock.html');
-        stream.pipe(res);
+        var ticker = url.pathname.slice(7);
+        // check if the ticker file exists.
+        fs.stat(root + '/QuandlQuery/tickers/' + ticker + '.json', function(err) {
+            if (err) {
+                if (err.code == 'ENOENT') send404(res);
+                else                      send500(res);
+            } 
+            else {
+                res.writeHead(200, {"Content-Type": "text/html"});
+                var stream = fs.createReadStream(root + '/_html/stock.html');
+                stream.pipe(res);
+            }
+        });
     }
             
     else {
         fs.stat(filepath, function(err, stats) {
             if (err) {
-                // File doesn't exist.
-                if (err.code == 'ENOENT') {
-                    send404(res);
-                } 
-                
-                else {
-                    res.statusCode = 500;
-                    res.end('Internal Server Error');
-                }
+                if (err.code == 'ENOENT') send404(res);
+                else                      send500(res);
             } 
-            
             else {
+                res.statusCode = 200;
                 var stream = fs.createReadStream(filepath);
                 stream.pipe(res);
             }
         });       
     }
     
-    function send404(response) {
-        response.statusCode = 404;
-        response.end('File Not Found');
+    function createError(errorCode, errorString) {
+        return function(response) {
+            response.statusCode = errorCode;
+            response.end(errorString);
+        }
     }
 });
+
 server.listen(3000);
 console.log('Server running at http://localhost:3000/');
