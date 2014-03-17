7139cbfe91fae4171f04068facdf74d6fb337cf7
BodkerJ <j.bodker@hotmail.com>
2014-03-17 09:32:59 -0600

    complete stock-list table

--- _js/stock-list.js
+++ _js/stock-list.js
@@ -20,18 +20,30 @@ function makeTable() {
     var url = "../QuandlQuery/alltickers.json";
     var data = new google.visualization.DataTable();    
     
+    var priceIndex = companyObject.column_names.indexOf('Stock Price');
+    var incomeIndex = companyObject.column_names.indexOf('Net Income');
+    var betaIndex = companyObject.column_names.indexOf('Value Line Beta');
+    var dyIndex = companyObject.column_names.indexOf('Dividend Yield');
+    
     data.addColumn('string', 'Name');
     data.addColumn('string', 'Ticker');
     data.addColumn('string', 'Exchange');
-    //data.addColumn('number', 'Stock Price');
+    data.addColumn('number', 'Stock Price');
+    data.addColumn('number', 'Net Income(in millions)');
+    data.addColumn('number', 'Beta');
+    data.addColumn('number', 'Dividend Yield');
     
     for(var i in companyObject) {
-        data.addRows([
-            [companyObject[i]['name'], companyObject[i]['ticker'], companyObject[i]['exchange']]            
-        ]);
+        if(i != 'column_names') {
+            data.addRows([
+                [companyObject[i]['name'], companyObject[i]['ticker'], companyObject[i]['exchange'],
+                 companyObject[i]['data'][priceIndex], companyObject[i]['data'][incomeIndex],
+                 companyObject[i]['data'][betaIndex], companyObject[i]['data'][dyIndex]]            
+            ]);
+        }
     }
 
     var table = new google.visualization.Table(document.getElementById('table_div'));
-    table.draw(data, {showRowNumber: true});
+    table.draw(data, {sortColumn: 0}, {sortAscending: true});
     
 }
